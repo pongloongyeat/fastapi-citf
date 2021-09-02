7a7f3db58aea77b130c04d14efbc9fe53eaac157
pongloongyeat <pongloongyeat@gmail.com>
2021-09-03 02:57:46 +0800

    Add docstrings

--- main.py
+++ main.py
@@ -12,26 +12,52 @@ except ImportError:
 
 app = FastAPI()
 
-vax_registration_malaysia_parser = CITFGitHubCSVParser(path_to_csv='registration/vaxreg_malaysia.csv')
-vax_registration_state_parser = CITFGitHubCSVParser(path_to_csv='registration/vaxreg_state.csv')
-vax_malaysia_parser = CITFGitHubCSVParser(path_to_csv='vaccination/vax_malaysia.csv')
-vax_state_parser = CITFGitHubCSVParser(path_to_csv='vaccination/vax_state.csv')
+# vax_registration_malaysia_parser = CITFGitHubCSVParser(path_to_csv='registration/vaxreg_malaysia.csv')
+# vax_registration_state_parser = CITFGitHubCSVParser(path_to_csv='registration/vaxreg_state.csv')
+# vax_malaysia_parser = CITFGitHubCSVParser(path_to_csv='vaccination/vax_malaysia.csv')
+# vax_state_parser = CITFGitHubCSVParser(path_to_csv='vaccination/vax_state.csv')
 
 
 @app.get('/')
 @app.get('/help')
 async def help() -> Dict:
-    return {"message": "Hello World"}
+    """Shows this help message."""
+
+    return {
+        '/': help.__doc__,
+        '/help': help.__doc__,
+        '/registration/malaysia': get_latest_registration_data_malaysia.__doc__,
+        '/registration/malaysia/latest': get_latest_registration_data_malaysia.__doc__,
+        '/registration/malaysia/{date}': get_registration_data_malaysia.__doc__,
+        '/vaccination/malaysia': get_latest_vax_data_malaysia.__doc__,
+        '/vaccination/malaysia/latest': get_latest_vax_data_malaysia.__doc__,
+        '/vaccination/malaysia/{date}': get_vax_data_malaysia.__doc__,
+        '/registration/state': get_latest_registration_data_state.__doc__,
+        '/registration/state/all/latest': get_latest_registration_data_state.__doc__,
+        '/registration/state/{state}/latest': get_latest_registration_data_for_state.__doc__,
+        '/registration/state/all/{date}': get_registration_data_all_state.__doc__,
+        '/registration/state/{state}/{date}': get_registration_data_state.__doc__,
+        '/vaccination/state': get_latest_vax_data_state.__doc__,
+        '/vaccination/state/all/latest': get_latest_vax_data_state.__doc__,
+        '/vaccination/state/{state}/latest': get_latest_vax_data_for_state.__doc__,
+        '/vaccination/state/all/{date}': get_vax_data_all_state.__doc__,
+        '/vaccination/state/{state}/{date}': get_vax_data_state.__doc__,
+    }
 
 #region Malaysia
 #region Vaccination registration statistics
 @app.get('/registration/malaysia')
 @app.get('/registration/malaysia/latest')
 async def get_latest_registration_data_malaysia() -> Union[Dict, None]:
+    """Returns the latest registration statistics for Malaysia."""
+
     return vax_registration_malaysia_parser.csv().tail(1).to_dict('records')[0]
 
 @app.get('/registration/malaysia/{date}')
 async def get_registration_data_malaysia(date: str) -> Union[Dict, None]:
+    """Returns the registration statistics for Malaysia
+    during a certain date with format YYYY-MM-DD."""
+
     dataframe = vax_registration_malaysia_parser.csv()
     matching = dataframe[dataframe['date'] == date].to_dict('records')
 
@@ -45,10 +71,15 @@ async def get_registration_data_malaysia(date: str) -> Union[Dict, None]:
 @app.get('/vaccination/malaysia')
 @app.get('/vaccination/malaysia/latest')
 async def get_latest_vax_data_malaysia() -> Dict:
+    """Returns the latest vaccination statistics for Malaysia."""
+
     return vax_malaysia_parser.csv().tail(1).to_dict('records')[0]
 
 @app.get('/vaccination/malaysia/{date}')
 async def get_vax_data_malaysia(date: str) -> Union[Dict, None]:
+    """Returns the vaccination statistics for Malaysia
+    during a certain date with format YYYY-MM-DD."""
+
     dataframe = vax_malaysia_parser.csv()
     matching = dataframe[dataframe['date'] == date].to_dict('records')
 
@@ -64,10 +95,16 @@ async def get_vax_data_malaysia(date: str) -> Union[Dict, None]:
 @app.get('/registration/state')
 @app.get('/registration/state/all/latest')
 async def get_latest_registration_data_state() -> Dict:
+    """Returns the latest registration statistics for all states."""
+
     return vax_registration_state_parser.csv().tail(16).to_dict('records')
 
 @app.get('/registration/state/{state}/latest')
 async def get_latest_registration_data_for_state(state: str) -> Union[Dict, None]:
+    """Returns the latest registration statistics for a specific state.
+    Note that this should follow the same naming scheme as that specified
+    in the CITF repository (see https://github.com/CITF-Malaysia/citf-public/blob/main/registration/vaxreg_state.csv)."""
+
     dataframe = vax_registration_state_parser.csv().tail(16)
     matching = dataframe[dataframe['state'] == state].to_dict('records')
 
@@ -77,7 +114,10 @@ async def get_latest_registration_data_for_state(state: str) -> Union[Dict, None
         raise HTTPException(status_code=404, detail=f'Record not found for state {state}')
 
 @app.get('/registration/state/all/{date}')
-async def get_registration_data_state(date: str) -> Union[Dict, None]:
+async def get_registration_data_all_state(date: str) -> Union[Dict, None]:
+    """Returns the registration statistics for all states
+    during a certain date with format YYYY-MM-DD."""
+
     dataframe = vax_registration_state_parser.csv()
     matching = dataframe[dataframe['date'] == date].to_dict('records')
 
@@ -88,6 +128,12 @@ async def get_registration_data_state(date: str) -> Union[Dict, None]:
 
 @app.get('/registration/state/{state}/{date}')
 async def get_registration_data_state(state: str, date: str) -> Union[Dict, None]:
+    """Returns the registration statistics for a
+    certain state during a certain date with format
+    YYYY-MM-DD. Note that this should follow the same
+    naming scheme as that specified in the CITF repository
+    (see https://github.com/CITF-Malaysia/citf-public/blob/main/registration/vaxreg_state.csv)."""
+
     dataframe = vax_registration_state_parser.csv()
     matching = dataframe[dataframe['date'] == date]
     matching = matching[matching['state'] == state].to_dict('records')
@@ -102,10 +148,16 @@ async def get_registration_data_state(state: str, date: str) -> Union[Dict, None
 @app.get('/vaccination/state')
 @app.get('/vaccination/state/all/latest')
 async def get_latest_vax_data_state() -> Dict:
+    """Returns the latest vaccination statistics for all states."""
+
     return vax_state_parser.csv().tail(16).to_dict('records')
 
 @app.get('/vaccination/state/{state}/latest')
 async def get_latest_vax_data_for_state(state: str) -> Union[Dict, None]:
+    """Returns the latest vaccination statistics for a specific state.
+    Note that this should follow the same naming scheme as that specified
+    in the CITF repository (see https://github.com/CITF-Malaysia/citf-public/blob/main/registration/vaxreg_state.csv)."""
+
     dataframe = vax_state_parser.csv().tail(16)
     matching = dataframe[dataframe['state'] == state].to_dict('records')
 
@@ -115,7 +167,10 @@ async def get_latest_vax_data_for_state(state: str) -> Union[Dict, None]:
         raise HTTPException(status_code=404, detail=f'Record not found for state {state}')
 
 @app.get('/vaccination/state/all/{date}')
-async def get_vax_data_state(date: str) -> Union[Dict, None]:
+async def get_vax_data_all_state(date: str) -> Union[Dict, None]:
+    """Returns the vaccination statistics for all states
+    during a certain date with format YYYY-MM-DD."""
+
     dataframe = vax_state_parser.csv()
     matching = dataframe[dataframe['date'] == date].to_dict('records')
 
@@ -126,6 +181,12 @@ async def get_vax_data_state(date: str) -> Union[Dict, None]:
 
 @app.get('/vaccination/state/{state}/{date}')
 async def get_vax_data_state(state: str, date: str) -> Union[Dict, None]:
+    """Returns the vaccination statistics for a
+    certain state during a certain date with format
+    YYYY-MM-DD. Note that this should follow the same
+    naming scheme as that specified in the CITF repository
+    (see https://github.com/CITF-Malaysia/citf-public/blob/main/registration/vaxreg_state.csv)."""
+
     dataframe = vax_state_parser.csv()
     matching = dataframe[dataframe['date'] == date]
     matching = matching[matching['state'] == state].to_dict('records')
